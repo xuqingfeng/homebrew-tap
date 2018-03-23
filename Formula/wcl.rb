ff1f5a5705373f25c7253bf19bd58668efd6a02d
xuqingfeng <js-xqf@hotmail.com>
2018-03-23 18:52:34 +0800

    Update sha

--- Formula/wcl.rb
+++ Formula/wcl.rb
@@ -1,9 +1,8 @@
 class Wcl < Formula
     desc "tool-kit for devops daily work"
-    homepage "https://wiredcraft.com"
-    url "https://github.com/xuqingfeng/took-kit/releases/download/v0.1.0/wcl-darwin-amd64"
-    version "0.1.0"
-    # sha256 ""
+    url "https://github.com/xuqingfeng/took-kit/releases/download/v0.0.7/wcl-darwin-amd64"
+    version "0.0.7"
+    sha256 "5fcc0479cd9ff7d7a37d016c1215a31ab0a634bead14ac43dba02dec362e222e"
 
     def install
       bin.install "wcl"
