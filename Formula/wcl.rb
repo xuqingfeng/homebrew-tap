5d69d643ba4a60614d602095f69168656174a9b3
xuqingfeng <js-xqf@hotmail.com>
2018-03-23 18:55:06 +0800

    Fix typo

--- Formula/wcl.rb
+++ Formula/wcl.rb
@@ -1,6 +1,6 @@
 class Wcl < Formula
     desc "tool-kit for devops daily work"
-    url "https://github.com/xuqingfeng/took-kit/releases/download/v0.0.7/wcl-darwin-amd64"
+    url "https://github.com/xuqingfeng/tool-kit/releases/download/v0.0.7/wcl-darwin-amd64"
     version "0.0.7"
     sha256 "5fcc0479cd9ff7d7a37d016c1215a31ab0a634bead14ac43dba02dec362e222e"
 
