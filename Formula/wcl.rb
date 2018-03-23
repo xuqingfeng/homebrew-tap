be8ff2eeab8e06fd0da9a456e940c5f6e8f91b42
xuqingfeng <js-xqf@hotmail.com>
2018-03-23 19:11:00 +0800

    Rename after download

--- Formula/wcl.rb
+++ Formula/wcl.rb
@@ -5,6 +5,7 @@ class Wcl < Formula
     sha256 "5fcc0479cd9ff7d7a37d016c1215a31ab0a634bead14ac43dba02dec362e222e"
 
     def install
+      system "mv wcl-darwin-amd64 wcl"
       bin.install "wcl"
     end
 
